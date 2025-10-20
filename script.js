6866ea0d2d2792a67565ea6b2c21ceec2b04958e
ASHIQUR RUBBIN TALEB AYON <67256635+ashiqur196@users.noreply.github.com>
2025-10-20 20:46:07 +0600

    First Commit
    First Date: 20-10-2025

---
+++ script.js
@@ -0,0 +1,4 @@
+// Displays the current year and last modified date
+
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("modified").textContent = document.lastModified;
